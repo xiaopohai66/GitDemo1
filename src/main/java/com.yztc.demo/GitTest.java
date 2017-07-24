9f3debfbed8170ce7c836a540ac6d33e5f223ef5
xiaopohai66 <853363223@qq.com>
2017-07-24 21:33:42 +0800

    我只是试一试行不行

---
+++ src/main/java/com.yztc.demo/GitTest.java
@@ -0,0 +1,14 @@
+package com.yztc.demo;
+
+/**
+ * Created by lbb on 2017/7/24.
+ */
+public class GitTest {
+
+    private static String name = null;
+
+    public static void main(String[] args) {
+
+        name="wangcai";
+    }
+}
